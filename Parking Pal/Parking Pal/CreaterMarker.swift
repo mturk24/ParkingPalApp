f98c131952119f9cdd6dbf726f884da2605e8a0f
Matt <mturk@berkeley.edu>
2016-12-07 17:35:57 -0800

    working on create marker view continued

--- Parking Pal/Parking Pal/CreaterMarker.swift
+++ Parking Pal/Parking Pal/CreaterMarker.swift
@@ -20,34 +20,34 @@ class CreaterMarker: UIViewController {
         var yCoord: UITextView
         var sendButton: UIButton
         
-        let camera = GMSCameraPosition.camera(withLatitude: 41.887,
-                                              longitude:-87.622,
+        let camera = GMSCameraPosition.camera(withLatitude: 37.8716,
+                                              longitude:122.2727,
                                               zoom:15)
         let mapView = GMSMapView.map(withFrame: .zero, camera:camera)
         
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: 41.887, longitude: -87.622)
-        marker.appearAnimation = kGMSMarkerAnimationPop
-        marker.icon = UIImage(named: "flag_icon")
-        marker.map = mapView
-        markers.append(marker)
-        marker.snippet = "Percent Full: 80%\nPrice: $5/hr"
-        
-        let marker2 = GMSMarker()
-        marker2.position = CLLocationCoordinate2D(latitude: 41.885, longitude: -87.622)
-        marker2.appearAnimation = kGMSMarkerAnimationPop
-        marker2.icon = UIImage(named: "flag_icon")
-        marker2.map = mapView
-        markers.append(marker2)
-        marker2.snippet = "Percent Full: 40%\nPrice: $7/hr"
-        
-        let marker3 = GMSMarker()
-        marker3.position = CLLocationCoordinate2D(latitude: 41.887, longitude: -87.621)
-        marker3.appearAnimation = kGMSMarkerAnimationPop
-        marker3.icon = UIImage(named: "flag_icon")
-        marker3.map = mapView
-        markers.append(marker3)
-        marker3.snippet = "Percent Full: 30%\nPrice: $15/hr"
+//        let marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2D(latitude: 41.887, longitude: -87.622)
+//        marker.appearAnimation = kGMSMarkerAnimationPop
+//        marker.icon = UIImage(named: "flag_icon")
+//        marker.map = mapView
+//        markers.append(marker)
+//        marker.snippet = "Percent Full: 80%\nPrice: $5/hr"
+//        
+//        let marker2 = GMSMarker()
+//        marker2.position = CLLocationCoordinate2D(latitude: 41.885, longitude: -87.622)
+//        marker2.appearAnimation = kGMSMarkerAnimationPop
+//        marker2.icon = UIImage(named: "flag_icon")
+//        marker2.map = mapView
+//        markers.append(marker2)
+//        marker2.snippet = "Percent Full: 40%\nPrice: $7/hr"
+//        
+//        let marker3 = GMSMarker()
+//        marker3.position = CLLocationCoordinate2D(latitude: 41.887, longitude: -87.621)
+//        marker3.appearAnimation = kGMSMarkerAnimationPop
+//        marker3.icon = UIImage(named: "flag_icon")
+//        marker3.map = mapView
+//        markers.append(marker3)
+//        marker3.snippet = "Percent Full: 30%\nPrice: $15/hr"
         
         view = mapView
     }
